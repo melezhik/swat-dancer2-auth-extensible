ea410b0b244073527fbdfed75b7f0131bd29ed48
Alexey Melezhik <melezhik@gmail.com>
2016-03-04 13:37:48 +0300

    salaries-check

---
+++ salaries-denied/hook.pm
@@ -0,0 +1,30 @@
+use swat::app;
+start_app();
+
+run_swat_module(
+    POST => '/logout'
+);
+
+run_swat_module(
+    POST => '/login' ,
+    {
+        username => 'foo',
+        password => 'new_secret',
+        return_url => '/report'
+
+    }
+);
+
+
+run_swat_module(
+    GET => '/salaries', 
+    {
+        'auth' => 0
+    }
+);
+
+set_response('done');
+
+1;
+
+
